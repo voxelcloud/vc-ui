f78e083cb4725a16944ebdfb1f4a00164adf96c0
HarryChen0506 <chenwenhua0506@sina.com>
2021-01-02 12:29:50 +0800

    feat: select component

---
+++ rollup.config.base.js
@@ -0,0 +1,33 @@
+import alias from 'rollup-plugin-alias'
+import { eslint } from 'rollup-plugin-eslint'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import babel from 'rollup-plugin-babel'
+import replace from 'rollup-plugin-replace'
+
+export default {
+  input: 'src/index.js',
+  // external: ['react', 'react-dom', 'react-is'],
+  external: id => id.includes('@material-ui') || ['react', 'react-dom', 'react-is'].includes(id) ,
+  plugins: [
+    alias({
+      resolve: ['.js']
+    }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+    }),
+    resolve(),
+    commonjs({
+      // non-CommonJS modules will be ignored, but you can also
+      // specifically include/exclude files
+      include: 'node_modules/**',
+    }),
+    eslint({
+      include: ['src/**/*.js']
+    }),
+    babel({
+      runtimeHelpers: true,
+      exclude: 'node_modules/**' // only transpile our source code
+    })
+  ]
+}
