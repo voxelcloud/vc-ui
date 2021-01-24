47cc2b3d98cce0dcfca1754fbd8475dbc46f13a5
HarryChen0506 <chenwenhua0506@sina.com>
2021-01-24 11:16:27 +0800

    fix: icon component color

--- src/Icon/Icon.js
+++ src/Icon/Icon.js
@@ -1,16 +1,45 @@
 import React from 'react'
 import t from 'prop-types'
 import MaIcon from '@material-ui/core/Icon'
-import { useTheme } from '@material-ui/core/styles'
+import { useTheme, makeStyles } from '@material-ui/core/styles'
 import iconMap from './iconMap'
 import clsx from 'clsx'
 
-const Icon = React.forwardRef(({ name, className, ...otherProps }, ref) => {
+const useStyles = makeStyles(theme => ({
+  warning: {
+    color: theme?.palette?.warning?.main
+  },
+  success: {
+    color: theme?.palette?.success?.main
+  },
+  info: {
+    color: theme?.palette?.info?.main
+  },
+  text: {
+    color: theme?.palette?.text?.main
+  }
+}))
+const customColor = ['warning', 'success', 'info', 'text']
+const Icon = React.forwardRef(({ name, className, color, ...otherProps }, ref) => {
   const theme = useTheme()
   const iconMapTotal = { ...iconMap, ...theme.iconMap }
+  const customClasses = useStyles()
+  const customClassname = clsx({
+    [iconMapTotal[name]]: name,
+    [customClasses.warning]: color === 'warning',
+    [customClasses.success]: color === 'success',
+    [customClasses.info]: color === 'info',
+    [customClasses.text]: color === 'text',
+    [className]: className
+  })
+  const colorProp = {}
+  if(!customColor.includes(color)) {
+    colorProp.color = color
+  }
   return <MaIcon
     ref={ref}
-    className={clsx(name && iconMapTotal[name], className)}
+    className={customClassname}
+    {...colorProp}
     {...otherProps}
   />
 })
@@ -18,6 +47,7 @@ const Icon = React.forwardRef(({ name, className, ...otherProps }, ref) => {
 Icon.propTypes = {
   name: t.string,
   className: t.string,
+  color: t.string
 }
 
 Icon.displayName = 'VcIcon'
