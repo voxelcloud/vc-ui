27333664414419e0badb4b6ac21eca1828daa7f8
HarryChen0506 <chenwenhua0506@sina.com>
2021-04-11 17:41:23 +0800

    perf: formUpdater component export

--- src/FormUpdater/style.js
+++ src/FormUpdater/style.js
@@ -1,4 +1,4 @@
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles'
 
 export const useStyles = makeStyles(() => ({
   root: {
@@ -35,4 +35,4 @@ export const useStyles = makeStyles(() => ({
       marginBottom: '30px',
     },
   },
-}));
+}))
